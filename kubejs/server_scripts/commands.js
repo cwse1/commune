d8e9828a6445d1a2f50ce15aa9afe6103ab8b346
cwse1 <73314023+cwse1@users.noreply.github.com>
2024-11-23 11:37:15 -0500

    update mods and add server command

--- kubejs/server_scripts/commands.js
+++ kubejs/server_scripts/commands.js
@@ -9,5 +9,14 @@ ServerEvents.commandRegistry(event => {
         i++;
         return i;
       })
+  );
+  event.register(
+    Commands.literal("shrink")
+      .requires(src => src.hasPermission(1))
+      .executes(c => {
+        let i = 0;
+        Utils.server.runCommand("scale set pehkui:base 0.25 @a");
+        return i;
+      })
   )
 })
