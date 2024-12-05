ecc05a251c2018e0dbe368e2e1e4235f5e110209
cwse1 <73314023+cwse1@users.noreply.github.com>
2024-12-05 10:38:30 -0500

    added grow command

--- kubejs/server_scripts/commands.js
+++ kubejs/server_scripts/commands.js
@@ -18,5 +18,14 @@ ServerEvents.commandRegistry(event => {
         Utils.server.runCommand("scale set pehkui:base 0.25 @p");
         return i;
       })
+  ); 
+  event.register(
+    Commands.literal("grow")
+      .requires(src => src.hasPermission(1))
+      .executes(c => {
+        let i = 0;
+        Utils.server.runCommand("scale set pehkui:base 1 @p");
+        return i;
+      })
   )
 })
