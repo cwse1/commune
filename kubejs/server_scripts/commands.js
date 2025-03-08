61d730a356b5994437af010b95827c67e00a095b
cwse1 <73314023+cwse1@users.noreply.github.com>
2025-03-08 07:45:48 -0500

    migrating to new machine

--- kubejs/server_scripts/commands.js
+++ kubejs/server_scripts/commands.js
@@ -11,21 +11,19 @@ ServerEvents.commandRegistry(event => {
       })
   );
   event.register(
-    Commands.literal("shrink")
+    Commands.literal("size")
       .requires(src => src.hasPermission(2))
-      .executes(c => {
-        let i = 0;
-        Utils.server.runCommand("scale set pehkui:base 0.25 @p");
-        return i;
-      })
+      .executes(c => size(c.source.player))
   ); 
   event.register(
     Commands.literal("grow")
       .requires(src => src.hasPermission(2))
-      .executes(c => {
-        let i = 0;
-        Utils.server.runCommand("scale set pehkui:base 1 @p");
-        return i;
-      })
+      .executes(c => size(c.source.player))
+      .then(Command.argument("scale", Arguments.STRING.create(event))
+        .then(Command.argument("target", Arguments.PLAYER.create(event)))
+      )
   )
+  let size = () => {
+    Utils.server.runCommand("scale set pehkui:base 1 @p");
+  } 
 })
